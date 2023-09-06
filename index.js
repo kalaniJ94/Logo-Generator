04609c86355074722f49c45df859c4814093bb43
kalaniJ94 <kalaniJ94@gmail.com>
2023-09-06 13:41:50 -0600

    shapes svg finished

--- index.js
+++ index.js
@@ -10,22 +10,43 @@ const questions = [
         message: "Please enter up to three characters to use in the logo.",
     },
     {
-        type: "input",
-        name: "color",
-        message: "Please enter a color for your logo (or a hexadecimal code)"
-    },
-    {
         type: "list",
         name: "shape",
         message: "What shape should your logo be?",
         choices: ["Circle", "Square", "Triangle"]
     },
+    {
+        type: "input",
+        name: "color",
+        message: "Please enter a color for your shape in your logo (or a hexadecimal code)"
+    },
+    { 
+        type: "input",
+        name: "letterColor",
+        message: "Please enter a color for the letters in your logo (or a hexadecimal code)"
+    },
 ]
+
+
+function createShape(letterColor, color, shape, letters){
+    
+    
+}
+
+
+
+
+
 function init() {
     inquirer
         .prompt(questions)
-        .then((data) => {
-            console.log(data);
+        .then((response) => {
+            console.log(response);
+            const data = logo(response)
+
+            console.log(data)
+            writeToFile('logo.svg', data, (err) => 
+            err? console.log(err) : console.log('Generated logo.svg'))
         })
 };
 
