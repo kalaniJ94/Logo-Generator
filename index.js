ab7cc425bc1f853461db1d3e197d6320ff58d49f
kalaniJ94 <kalaniJ94@gmail.com>
2023-09-07 11:37:12 -0600

    logo.svg generated, data issues

--- index.js
+++ index.js
@@ -1,66 +1,45 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 
-// const { choices } = require('yargs');
-// const { generateSVG } = require();
+const { SVGGenerator, generateSVG  } = require('./lib/shapes.js');
 
 
 const questions = [
     {
-        type: "input",
-        name: "letters",
-        message: "Please enter up to three characters to use in the logo.",
-    },
-    {
         type: "list",
         name: "shape",
         message: "What shape should your logo be?",
-        choices: ["Circle", "Square", "Triangle"]
+        choices: ["Circle", "Rectangle", "Triangle"]
     },
     {
         type: "input",
         name: "color",
         message: "Please enter a color for your shape in your logo (or a hexadecimal code)"
     },
+    {
+        type: "input",
+        name: "letters",
+        message: "Please enter up to three characters to use in the logo.",
+    },
     { 
         type: "input",
-        name: "letterColor",
+        name: "textColor",
         message: "Please enter a color for the letters in your logo (or a hexadecimal code)"
     },
 ]
 
 
-// function createShape(letterColor, color, shape, letters){
-// [ {
-//     name: ('circle'),{
-     
-//      <svg width="200" height="100" xmlns="http://www.w3.org/2000/svg">
-//     <circle cx="90" cy="50" r="50" fill="${color}"></circle>
-//         <text  x="75" y="50" font-size="16" fill="white">KDJ</svg>
-// </svg>
-
-//     }
-// }]
-    
-// }
-
-function writeToFile(filename, data) {
-    fs.writeFile(filename, data, (err) => 
-    err ? console.log(err) : console.log('Check out your logo!'))
-};
-
-
-
 function init() {
     inquirer
-        .prompt(questions)
-        .then((response) => {
-            console.log(response);
-            const data = logo(response)
-
-            console.log(data)
-            writeToFile('logo.svg', data, (err) => 
-            err? console.log(err) : console.log('Generated logo.svg'))
+    .prompt(questions)
+    .then((data) => {
+        console.log(data);
+        let logoData = generateSVG(data.shape, data.color, data.letters, data.textColor);
+        const filename = 'logo.svg'
+        
+        fs.writeFile(filename, logoData, (err) => 
+        err ? console.log(err) : console.log('Check out your logo!'))
+      
         })
 };
 
--- test/jest.setup.js
+++
@@ -1,8 +0,0 @@
-const {JSDOM} = require('jsdom');
-const { toHaveAttribute, toHaveTextContent, } = require('@testing-library/jest-dom/matchers');
-
-
-expect.extend({toHaveAttribute, toHaveTextContent});
-
-
-//used to setup jest code globally, make testing code cleaner. Jest has specified this file in "setupFiles", in package.json. 
