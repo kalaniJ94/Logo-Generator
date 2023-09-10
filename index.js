fdd1c9bed5e5a4649647be4e6b1c45b78cae115a
kalaniJ94 <kalaniJ94@gmail.com>
2023-09-10 12:52:33 -0600

    new generation logic

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
-
-const { SVGGenerator, generateSVG } = require('./lib/shapes.js');
+const { Circle, Triangle, Square } = require("./lib")
+// const { SVGGenerator, generateSVG } = require('./lib/shapes.js');
 
 
 const questions = [
@@ -21,7 +21,7 @@ const questions = [
         name: "letters",
         message: "Please enter up to three characters to use in the logo.",
     },
-    { 
+    {
         type: "input",
         name: "textColor",
         message: "Please enter a color for the letters in your logo (or a hexadecimal code)"
@@ -31,21 +31,49 @@ const questions = [
 
 function init() {
     inquirer
-    .prompt(questions)
-    .then((data) => {
-        console.log(data);
-        if(data.letters.length != 3){
-            throw new Error("Please enter three letters for the logo.");
-        };
-        const color = data.color.toLowerCase();
-        const textColor = data.textColor.toLowerCase();
-        let logoData = generateSVG(data.shape, color, textColor, data.letters,);
-        const filename = 'logo.svg'
-        
-        fs.writeFile(filename, logoData, (err) => 
-        err ? console.log(err) : console.log('Check out your logo!'))
-      
+        .prompt(questions)
+        .then((data) => {
+            console.log(data);
+            compileSVG(data.shape, data);
         })
+    // if(data.letters.length != 3){
+    //     throw new Error("Please enter three letters for the logo.");
+    // };
+
+    // const color = data.color.toLowerCase();
+    // const textColor = data.textColor.toLowerCase();
+    // let logoData = generateSVG(data.shape, color, textColor, data.letters,);
+
+
 };
+
+function compileSVG(shape, data) {
+    console.log(data);
+    if (shape === "Circle") {
+        const circle = new Circle(data.color, data.textColor, data.letters)
+        let SVGString = circle.render() + circle.insertCircleSVG() + circle.insertText()
+        const filename = 'logo.svg'
+        fs.writeFile(filename, SVGString, (err) =>
+            err ? console.log(err) : console.log('Check out your logo!'))
+
+
+    }
+    else if (shape === "Triangle") {
+        const triangle = new Triangle(data.color, data.textColor, data.letters)
+
+        let SVGString = triangle.render() + triangle.insertTriangleSVG() + triangle.insertText();
+        const filename = 'logo.svg'
+        fs.writeFile(filename, SVGString, (err) =>
+            err ? console.log(err) : console.log('Check out your logo!'))
+
+    }
+    else if (shape === "Rectangle"){
+        const rectangle = new Square(data.color, data.textColor, data.letters)
+        let SVGString = rectangle.render() + rectangle.insertSquareSVG() + rectangle.insertText();
+        const filename = 'logo.svg'
+        fs.writeFile(filename, SVGString, (err) =>
+            err ? console.log(err) : console.log('Check out your logo!'))
+    }
+}
 
 init();
